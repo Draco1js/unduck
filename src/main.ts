4b9a47b8c1d071aab0e50d9e445fcb458b56ef0b
Draco <thatdracoyt@gmail.com>
2025-04-08 18:41:46 +0500

    Update bang order support
    Made it so bangs work both ways, before and after, just like the actual duckduckgo, aka: !g and g! are both valid bangs for google, and !a and a! are both valid bangs for amazon

--- src/main.ts
+++ src/main.ts
@@ -55,13 +55,13 @@ function getBangredirectUrl() {
     return null;
   }
 
-  const match = query.match(/!(\S+)/i);
+  const match = query.match(/(\S+!)|(!\S+)/i);
 
   const bangCandidate = match?.[1]?.toLowerCase();
   const selectedBang = bangs.find((b) => b.t === bangCandidate) ?? defaultBang;
 
   // Remove the first bang from the query
-  const cleanQuery = query.replace(/!\S+\s*/i, "").trim();
+  const cleanQuery = query.replace(/(!\S+\s*)|(\S+!\s*)/i, "").trim();
 
   // Format of the url is:
   // https://www.google.com/search?q={{{s}}}
